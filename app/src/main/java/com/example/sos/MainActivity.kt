9c1fd06eed3163a7250028fb83ec92e768212117
hexis <mailrusipusi@mail.ru>
2024-06-21 12:15:11 +0300

    add user phone number

--- app/src/main/java/com/example/sos/MainActivity.kt
+++ app/src/main/java/com/example/sos/MainActivity.kt
@@ -7,6 +7,8 @@ import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import android.widget.Toast
 import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.ContextCompat
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
@@ -14,24 +16,27 @@ import com.yandex.mapkit.geometry.Point
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.Task
-import androidx.core.content.ContextCompat
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.map.MapObjectCollection
 import com.yandex.mapkit.map.PlacemarkMapObject
 import com.yandex.runtime.image.ImageProvider
 import okhttp3.*
 import java.io.IOException
-
-
+import java.util.regex.Pattern
 
 class MainActivity : ComponentActivity() {
     lateinit var mapview: MapView
     lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var mapObjects: MapObjectCollection
     private lateinit var userMarker: PlacemarkMapObject
+    private lateinit var phoneNumberEditText: EditText
+    private lateinit var sosButton: Button
     private val LOCATION_PERMISSION_REQUEST_CODE = 1
     private val TELEGRAM_BOT_TOKEN = "7413186571:AAGliCfbCYIVYWdPGaVjIcbEh8JJ2jZ49Ks"
     private val CHAT_ID = "711136853"
+    private var isPhoneNumberInputVisible = false
+    private var userLatitude: Double? = null
+    private var userLongitude: Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,15 +44,29 @@ class MainActivity : ComponentActivity() {
         MapKitFactory.initialize(this)
         setContentView(R.layout.fragment_main_window)
         mapview = findViewById(R.id.mapview)
+        phoneNumberEditText = findViewById(R.id.phoneNumberEditText)
+        sosButton = findViewById(R.id.sosButton)
+
         mapObjects = mapview.map.mapObjects.addCollection()
         mapview.map.move(
             CameraPosition(Point(43.682770, 40.266281), 14.0f, 0.0f, 0.0f),
             Animation(Animation.Type.SMOOTH, 3f), null
         )
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        val sosButton = findViewById<Button>(R.id.sosButton)
+
         sosButton.setOnClickListener {
-            getCurrentLocation()
+            if (isPhoneNumberInputVisible) {
+                val phoneNumber = phoneNumberEditText.text.toString()
+                if (userLatitude != null && userLongitude != null && isValidPhoneNumber(phoneNumber)) {
+                    sendLocationToTelegramBot(userLatitude!!, userLongitude!!, phoneNumber)
+                } else {
+                    Toast.makeText(this, "Пожалуйста, введите корректный номер телефона", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                phoneNumberEditText.visibility = EditText.VISIBLE
+                isPhoneNumberInputVisible = true
+                getCurrentLocation()
+            }
         }
 
         checkLocationPermission()
@@ -62,7 +81,6 @@ class MainActivity : ComponentActivity() {
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 LOCATION_PERMISSION_REQUEST_CODE
             )
-        } else {
         }
     }
 
@@ -95,7 +113,9 @@ class MainActivity : ComponentActivity() {
             if (task.isSuccessful) {
                 val location = task.result
                 if (location != null) {
-                    val userLocation = Point(location.latitude, location.longitude)
+                    userLatitude = location.latitude
+                    userLongitude = location.longitude
+                    val userLocation = Point(userLatitude!!, userLongitude!!)
                     mapview.map.move(
                         CameraPosition(userLocation, 14.0f, 0.0f, 0.0f)
                     )
@@ -110,7 +130,6 @@ class MainActivity : ComponentActivity() {
                     Toast.makeText(
                         this, "Ваша позиция: (${location.latitude}, ${location.longitude})", Toast.LENGTH_LONG
                     ).show()
-                    sendLocationToTelegramBot(location.latitude, location.longitude)
                 } else {
                     Toast.makeText(this, "Включите геолокацию", Toast.LENGTH_SHORT).show()
                 }
@@ -121,14 +140,16 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun sendLocationToTelegramBot(latitude: Double, longitude: Double) {
-        val url = "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendLocation"
+    private fun sendLocationToTelegramBot(latitude: Double, longitude: Double, phoneNumber: String) {
+        val url = "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
         val client = OkHttpClient()
+
+        val mapUrl = "https://maps.yandex.ru/1.x/?ll=$longitude,$latitude&z=14&size=450,450&l=map&pt=$longitude,$latitude,pm2rdm"
+        val message = "Телефон: $phoneNumber\nКарта: $mapUrl"
 
         val requestBody = FormBody.Builder()
             .add("chat_id", CHAT_ID)
-            .add("latitude", latitude.toString())
-            .add("longitude", longitude.toString())
+            .add("text", message)
             .build()
 
         val request = Request.Builder()
@@ -151,6 +172,10 @@ class MainActivity : ComponentActivity() {
                         Toast.makeText(
                             this@MainActivity, "Местоположение отправлено, ожидайте", Toast.LENGTH_SHORT
                         ).show()
+                        // Обнуляем и скрываем поле ввода номера телефона
+                        phoneNumberEditText.setText("")
+                        phoneNumberEditText.visibility = EditText.GONE
+                        isPhoneNumberInputVisible = false
                     }
                 } else {
                     runOnUiThread {
@@ -163,6 +188,14 @@ class MainActivity : ComponentActivity() {
         })
     }
 
+    private fun isValidPhoneNumber(phoneNumber: String): Boolean {
+        // Регулярное выражение для проверки стандартного номера телефона России
+        val pattern = Pattern.compile("^((\\+7|7|8)+([0-9]){10})\$")
+        return pattern.matcher(phoneNumber).matches()
+    }
+
+
+
     override fun onStop() {
         mapview.onStop()
         MapKitFactory.getInstance().onStop()
@@ -174,8 +207,4 @@ class MainActivity : ComponentActivity() {
         MapKitFactory.getInstance().onStart()
         mapview.onStart()
     }
-
 }
-
-
-
